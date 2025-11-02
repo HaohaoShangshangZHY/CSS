677072001728e1cd1b2c5e63a585c9e807e7c6cc
Niko <18752676092@163.com>
2025-11-02 17:06:32 +0800

    20232099

---
+++ src/components/EventCard.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { type Event } from '../types'
+import EventMeta from './EventMeta.vue'
+
+defineProps<{
+  event: Event
+}>()
+</script>
+
+<template>
+  <RouterLink class="event-link" :to="{ name: 'event-detail-view', params: { id: event.id } }">
+    <div class="cursor-pointer border border-gray-600 p-[20px] w-[250px] mb-[18px] hover:scale-101 hover:shadow-sp">
+      <h2>{{ event.title }}</h2>
+      <span>@{{ event.time }} on {{ event.date }}</span>
+      <EventMeta :event="event" />
+    </div>
+  </RouterLink>
+</template>
+
+<style scoped>
+
+.event-link {
+  text-decoration: none;
+  color: #2c3e50;
+}
+</style>
